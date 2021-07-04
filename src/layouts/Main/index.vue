878cc311b8dfb7c7bb3f4d0c23302c9b2eee39e8
Mostafa <mostafa.rezaie@email.kntu.ac.ir>
2021-07-04 05:22:51 +0100

    adding index vue file

---
+++ src/layouts/Main/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+       <h1>main layout  :</h1>
+       <div>
+           <slot></slot>
+       </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'Home'
+}
+</script>
