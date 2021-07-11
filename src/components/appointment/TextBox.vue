b60e0b276b25be94b769eeffc8c0d9600cd56a26
Mostafa <mostafa.rezaie@email.kntu.ac.ir>
2021-07-11 14:46:03 +0100

    add time cards

---
+++ src/components/appointment/TextBox.vue
@@ -0,0 +1,71 @@
+<template >
+  <div class="wrapper" :class="getClass" @click="clickHandler">
+    <div class="text">
+      {{ text }}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: "title",
+    },
+    isActivatedBefore: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      active: false,
+    };
+  },
+  computed: {
+    getClass() {
+      return {
+        active: this.active,
+      };
+    },
+  },
+  methods: {
+    clickHandler() {
+      //chech if it is activated before or not
+      if (!this.isActivatedBefore) {
+        this.changeState();
+      } else {
+        if (this.active) {
+          this.changeState();
+        }
+      }
+    },
+    changeState() {
+      this.active = !this.active;
+      this.$emit("passState", this.active);
+    },
+  },
+};
+</script>
+<style scoped>
+.wrapper {
+  background-color: #f8f9fa;
+  width: 248px;
+  height: 46px;
+  display: inline-block;
+  text-align: center;
+  box-sizing: content-box;
+  border: 2px solid #f8f9fa;
+  cursor: pointer;
+}
+.text {
+  font-size: 20px;
+  margin-top: 12px;
+  font-weight: 300;
+  user-select: none;
+}
+.active {
+  border: solid #0a5bff 2px;
+  background-color: #d0dfff;
+}
+</style>
