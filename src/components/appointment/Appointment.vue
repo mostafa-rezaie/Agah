6de51d1fc0e3c225d9bd2141d4a2de31048e4d1c
Mostafa <mostafa.rezaie@email.kntu.ac.ir>
2021-07-11 14:45:36 +0100

    handle title show

--- src/components/appointment/Appointment.vue
+++ src/components/appointment/Appointment.vue
@@ -2,7 +2,12 @@
   <div>
     <div class="container-box">
       <app-header></app-header>
-        <router-view></router-view>
+      <div v-if="textContainerHandler" class="text-container">
+        <div class="title">شهر مورد نظر را انتخاب کنید</div>
+        <div class="subtitle">شعبه مورد نظر خود را انتخاب یا جستجو کنید</div>
+      </div>
+      <router-view></router-view>
+
       <app-side-bar></app-side-bar>
     </div>
   </div>
@@ -11,11 +16,20 @@
 <script>
 import Header from "../Header.vue";
 import SideBar from "./SideBar.vue";
-
 export default {
+  data() {
+    return {
+      inChooseCity : false
+    }
+  },
   components: {
     appSideBar: SideBar,
     appHeader: Header,
+  },
+  computed: {
+    textContainerHandler(){
+      return this.inChooseCity;
+    }
   },
 };
 </script>
@@ -25,5 +39,18 @@ export default {
   width: 80%;
   left: 0;
 }
-
+.text-container {
+  margin-right: 97px;
+  direction: rtl;
+  margin-top: 82px;
+}
+.title {
+  margin-bottom: 21px;
+  font-size: 24px;
+  font-weight: bolder;
+}
+.subtitle {
+  margin-right: 8px;
+  font-size: 20px;
+}
 </style>
