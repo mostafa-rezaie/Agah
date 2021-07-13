c31c3919886a6baac9d84709bee2e672201f2e36
Mostafa <mostafa.rezaie@email.kntu.ac.ir>
2021-07-13 15:27:18 +0100

    configure Vuex

--- src/main.js
+++ src/main.js
@@ -1,18 +1,30 @@
-import Vue from 'vue'
-import App from './App.vue'
-import "bootstrap/dist/css/bootstrap-grid.min.css"
-import './assets/css/style.css'
-import VueRouter from 'vue-router'
-import { routes } from './routes'
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap-grid.min.css";
+import "./assets/css/style.css";
+import VueRouter from "vue-router";
+import { routes } from "./routes";
+import axios from "axios";
+import { store } from "./store/store.js";
+
+Vue.config.productionTip = false;
+
+Vue.use({
+  install(Vue) {
+    Vue.prototype.$api = axios.create({
+      baseURL: "https://agah-admission-api.webjarprojects.ir/api/",
+    });
+  },
+});
 
 Vue.use(VueRouter);
 const router = new VueRouter({
   routes,
-  mode : 'history'
-})
+  mode: "history",
+});
 
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
   router,
-}).$mount('#app')
+  store,
+}).$mount("#app");
